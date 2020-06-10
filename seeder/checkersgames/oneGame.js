bdfabc2976b4d16ab0401014f0e4ad0f7bc26250
Conrado <zaolen@gmail.com>
2020-06-10 16:48:34 +0200

    bug fix: troubles with updating new turn

--- seeder/checkersgames/oneGame.js
+++ seeder/checkersgames/oneGame.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
 module.exports = {
+	"_id": ObjectId("5ee0d5d31ee3502a809c14ba"),
 	"playerOne": "User",
 	"playerTwo": "Admin",
 	"initiated": true,
 	"winner": "",
 	"drawOffered": false,
-	"gamePlay": ObjectId("5ed79e1dfb5fab1d08c60490")
+	"createdAt": "2020-06-10T12:45:07.958Z",
+	"updatedAt": "2020-06-10T12:45:22.833Z",
+	"__v": 0,
+	"gamePlay": ObjectId("5ee0d5e21ee3502a809c14bb")
 }
