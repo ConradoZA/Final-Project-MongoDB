d14748127235fd93d5a58f2af04ca03cfdcf8ab0
Conrado <zaolen@gmail.com>
2020-06-21 21:29:29 +0200

    Options not getting through

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const mongo_URI =
     process.env.NODE_ENV === 'production' ?
-        'mongodb+srv://Conrado:JBIDqc8q4DRCgeeA@cza-cko71.mongodb.net/play-two-games?retryWrites=true&w=majority' : 'mongodb://localhost:27017/play-games';
+        'jaja' : 'mongodb://localhost:27017/play-games';
 
-mongoose.connect('mongodb+srv://Conrado:JBIDqc8q4DRCgeeA@cza-cko71.mongodb.net/play-two-games?retryWrites=true&w=majority', {
+mongoose.connect(mongo_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
