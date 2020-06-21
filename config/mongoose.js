e5ef09e6760c03197526487f2a64442d9c518937
Conrado <zaolen@gmail.com>
2020-06-21 21:25:46 +0200

    Ok, I withdraw

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongo_URI =
+    process.env.NODE_ENV === 'production' ?
+        'mongodb+srv://Conrado:JBIDqc8q4DRCgeeA@cza-cko71.mongodb.net/play-two-games?retryWrites=true&w=majority' : 'mongodb://localhost:27017/play-games';
+
+mongoose.connect('mongodb+srv://Conrado:JBIDqc8q4DRCgeeA@cza-cko71.mongodb.net/play-two-games?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => console.log('Successfully conected to MongoDB'))
+    .catch(console.error)
